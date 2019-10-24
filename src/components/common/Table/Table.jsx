3e073e2651f6fa895a8c5915dab35d23ab6a36dd
vlad-komanchy <vkomanchy@gmail.com>
2019-10-24 23:50:11 +0300

    feat: add contentful, change preact version

--- src/components/common/Table/Table.jsx
+++ src/components/common/Table/Table.jsx
@@ -1,43 +1,63 @@
-import data from './tableData.json';
+import { useState, useEffect } from 'preact/hooks';
+import keys from './keys.json';
 
-const rows = data.map((row) => <tr>
-  <td>{row.dayOfWeek}</td>
-  <td>{row.time}</td>
-  <td>{row.typeOfTraining}</td>
-  <td>{row.placeForTraining}</td>
-</tr>);
-
-const columns = [
-  {
-    name: 'dayOfWeek',
-    displayName: 'День недели',
-  },
-  {
-    name: 'time',
-    displayName: 'Время',
-  },
-  {
-    name: 'typeOfTraining',
-    displayName: 'Тип тренировки',
-  },
-  {
-    name: 'placeForTraining',
-    displayName: 'Место проведения',
-  }
-];
+const contentful = require('contentful');
+const client = contentful.createClient({ ...keys });
 
 const TableComponent = () => {
-  return <table className='table'>
-    <thead>
-      <tr>
-        {columns.map((column) => <td>{column.displayName}</td>)}
-      </tr>
-    </thead>
+  const [data, setData] = useState([]);
 
-    <tbody>
-      {rows.map((row) => row)}
-    </tbody>
-  </table>
-};
+  useEffect(() => {
+    client
+      .getEntries('training')
+      .then((entry) => {
+        setData(entry.items.map(({ fields }) => fields));
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+
+  const rows = data.map((row) => <tr>
+    <td>{row.dayOfWeek}</td>
+    <td>{row.time}</td>
+    <td>{row.typeOfTraining}</td>
+    <td>{row.placeForTraining}</td>
+  </tr>
+  );
+
+  const columns = [
+    {
+      name: 'dayOfWeek',
+      displayName: 'День недели',
+    },
+    {
+      name: 'time',
+      displayName: 'Время',
+    },
+    {
+      name: 'typeOfTraining',
+      displayName: 'Тип тренировки',
+    },
+    {
+      name: 'placeForTraining',
+      displayName: 'Место проведения',
+    }
+  ];
+
+  return (
+    <table className='table'>
+      <thead>
+        <tr>
+          {columns.map((column) => <td>{column.displayName}</td>)}
+        </tr>
+      </thead>
+
+      <tbody>
+        {rows.map((row) => row)}
+      </tbody>
+    </table>
+  );
+}
+
 
 export default TableComponent;
