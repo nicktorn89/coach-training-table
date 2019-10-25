d818b24040e4ee57b4b810baab458658238cf7cd
vlad-komanchy <vkomanchy@gmail.com>
2019-10-25 23:59:27 +0300

    feat: add basic popup window with description

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,9 +1,11 @@
 import TableComponent from './Table';
 import Heading from './Heading';
 import Description from './Description';
+import TrainingDescription from './TrainingDescription';
 
 export {
   TableComponent as Table,
   Heading,
   Description,
+  TrainingDescription,
 };
